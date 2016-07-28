a2af480e1709bd2f4da84bd5abf6c3e4d2cc467b
Raphael Arantes <Raphael Arantes>
2016-07-27 22:02:10 -0300

    First commit

---
+++ ProjectSW01/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectSW
+{
+    class Program
+    {
+        /// <summary>
+        /// Coding in Star Wars theme
+        /// </summary>
+        /// Author:     rapharma
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Star Wars characters's characteristics :\n");
+
+            string[] characters = { "Luke", "R2-D2", "Darth", "Han" };
+
+            foreach (string s in characters)
+            {
+                if (s == "Luke" || s == "Darth")
+                {
+                    Console.WriteLine(s + " has the force");
+                }
+                else
+                {
+                    Console.WriteLine(s + " doesn't have the force");
+                }
+            }
+
+            Console.WriteLine("\nLearn to Code you should! ;)");
+
+
+        }
+    }
+    }
+
+
+
+
+
+
